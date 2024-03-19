11e092ff0bb45f949f79d675d40094c4bf7f682c
Tardanoir <tardanoir@gmail.com>
2024-03-19 11:20:18 -0300

    nvim updates

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -23,7 +26,6 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -2,15 +2,15 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
         require('kanagawa').setup({
-            compile = false,             -- enable compiling the colorscheme
-            undercurl = true,            -- enable undercurls
+            compile = false,  -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
             commentStyle = { italic = true },
             functionStyle = {},
-            keywordStyle = { italic = true},
+            keywordStyle = { italic = true },
             statementStyle = { bold = true },
-            transparent = true,         -- do not set background color
-            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+            transparent = true,    -- do not set background color
+            dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true, -- define vim.g.terminal_color_{0,17}
             colors = {
                 theme = {
                     all = {
@@ -22,7 +22,6 @@ return {
             }
         })
 
-		vim.cmd("colorscheme kanagawa")
-end
+        vim.cmd("colorscheme kanagawa")
+    end
 }
-
